189fc4324cb37591894b59e7b2a0b5a71aa3051e
shellaeffertz <covowakafoj66@gmail.com>
2024-11-05 19:18:35 +0100

    fix link

--- public/build/assets/MyPurchases-DvuIrMRY.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./AppLayout-BO8O399l.js";import{c as s,w as a,o as r,a as o}from"./app-ByAlqzdR.js";const f={__name:"MyPurchases",setup(c){return(_,t)=>(r(),s(e,{title:"My Purchases"},{default:a(()=>t[0]||(t[0]=[o("h1",{class:"w-fit mx-auto text-3xl text-red-500 mt-10"},"My Purchases",-1)])),_:1}))}};export{f as default};
